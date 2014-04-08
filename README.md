6e5fe18f7c043dfd6f9bb5edeb44905f2ea1e07c
Mikhail Dubov <mdubov@gmail.com>
2014-04-08 11:33:58 +0100

    Added focus event to tweet area, cleaned up CSS

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-twitter-front-end
+Twitter Front-End Replication
 =================
 
 Week 7 test @ Makers Academy
 
 The task of this test was to recreate Twitter's home page front-end.
+
+Working example: 
--- twitter-clone.js
+++ twitter-clone.js
@@ -8,6 +8,17 @@
      }
     }
     $(document).ready(function(){
+     
+     $('.tweet-content').focus(function(){
+      $('.new-tweet-footer').css('display', 'initial');
+      $('.tweet-content').css('height', '100px');
+     });
+      $('.tweet-content').focusout(function(){
+      $('.new-tweet-footer').css('display', 'none');
+      $('.tweet-content').css('height', '20px');
+     });
+
+
      addTenTweets()
 
      $(window).scroll(function () {
